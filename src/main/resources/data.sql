1f1399798818264db178f99595daee18952189fa
Rankatanka <hegro8348@oslomet.no>
2024-04-10 09:20:40 +0200

    Fixed sql initialization, creating tables, etc.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO Movies (movie) VALUES
+('Shrek'),
+('Shrek 2'),
+('Shrek the Third'),
+('Shrek Forever After');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,21 @@
+/* Creates table Movies, it's a table for movies, filled when the app is started */
+
+CREATE TABLE Movies (
+    movie VARCHAR(255),
+    PRIMARY KEY (movie)
+);
+
 /* Creates table Purchases, which mimics the Purchase class when the app is activated */
+/* The Movies table is referenced here */
 
 CREATE TABLE Purchases (
     id INTEGER AUTO_INCREMENT,
-    movie VARCHAR(255) NOT NULL,
+    movie VARCHAR(255),
     ticketAmount INTEGER NOT NULL,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
     phoneNumber VARCHAR(10) NOT NULL,
     mail VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (movie) REFERENCES Movies(movie)
 );
